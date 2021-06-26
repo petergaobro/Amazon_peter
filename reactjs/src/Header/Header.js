bdb181758dc85dddf8a276a598665ab29d1f1ed5
petergaobro <gaopeng19931027@gmail.com>
2021-06-26 15:29:33 +1000

    update

---
+++ reactjs/src/Header/Header.js
@@ -0,0 +1,19 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react';
+import '../Header/Header.css'
+
+function header() {
+    return (
+        <div className='header'>
+            <img 
+                className = "header_logo"
+                src="https://pngimg.com/image/61249"
+            />
+            <div className="header_search">
+                <input className="header_search_input" type="text"></input>
+            </div>
+        </div>
+    )
+}
+
+export default header
---
+++ reactjs/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAATO4LmB0OYIesbg8q0x3X8UzTyNMdN18",
+    authDomain: "peter-shopping-mall.firebaseapp.com",
+    projectId: "peter-shopping-mall",
+    storageBucket: "peter-shopping-mall.appspot.com",
+    messagingSenderId: "475033908610",
+    appId: "1:475033908610:web:d0c8cdc54261c77e66cb9b",
+    measurementId: "G-XVX8FWG2H0"
+};
