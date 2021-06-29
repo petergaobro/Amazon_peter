6da82bb51af10b53e6a6cc9259ad2947dc7dad6d
petergaobro <gaopeng19931027@gmail.com>
2021-06-29 10:54:16 +1000

    update

--- reactjs/src/Header/Header.js
+++ reactjs/src/Header/Header.js
@@ -5,12 +5,17 @@ import '../Header/Header.css'
 function header() {
     return (
         <div className='header'>
-            <img 
-                className = "header_logo"
+            <img
+                className="header_logo"
                 src="https://pngimg.com/image/61249"
             />
             <div className="header_search">
                 <input className="header_search_input" type="text"></input>
+                {/* logo */}
+            </div>
+
+            <div className="header_nav">
+
             </div>
         </div>
     )
